3bfe9bcbc8c49ff702636c0c71a0051fcd0044c3
LwaziMgazi <34287957+LwaziMgazi@users.noreply.github.com>
2025-07-03 22:22:10 +0200

    Adding Chamber page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,7 +6,8 @@ import {DOCUMENT} from '@angular/common';
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent implements OnInit {
-
+   currentTitle:any;
+   currentContent: string ='';
    constructor(@Inject(DOCUMENT) private readonly document: Document) {
       }
 
@@ -21,4 +22,29 @@ export class AboutUsComponent implements OnInit {
         script.async = true;
         this.document.head.appendChild(script);
     }
+
+    addContenttoModal(title: string) {
+      this.currentTitle = title;
+      if(title === 'INVESTMENT APPROACH') {
+       this.currentContent = `Khon’Structure has two tiers to its approach. The first is the construction
+       and leasing arm ofthe company, where we do rigorous demand analysis before acquiring plant needed for ourproject pipeline.
+       Once assets have been identified, investors are given an opportunity forfractional ownership. This fractional ownership gives them
+       rights to share in the rental incomeproportional to their stake. To ensure full
+        optimization of capital and assets, we only look forreputable clients/departments and projects in reasonably safe areas.`
+      } else if(title === 'INDUSTRY EXPERTISE'){
+        this.currentContent = `Khon’Structure is made great by the highly capable people behind it. Ndabenhle,
+        the founderand MD of Khon’Structure is also the founder of travel tech company AirStudent Travel and aM&G
+        200 Young Person of the Year. Ndabenhle is an Allan Gray fellow and his ties to thecommunity played a role in
+         Khon’Structure securing funding from E2 Investments. The realsecret sauce is in the people behind the scenes -
+         Mxolisi Ntshangase is the founder and CEO ofUmzuzo, a leading Engineering Consultancy in KZN. Malusi Ntshangase PR Eng,
+          who playsan advisory role is currently the Chief Engineer at the Department of Transport KZN.`
+      }else if( title === 'COMMUNITY IMPACT'){
+        this.currentContent = `Not only do we combat equipment scarcity in infrastructure development,
+        we also look toensure that our communities benefit financially from the development that is happeningaround them.
+        The mission is to use construction as a way to grow financial involvement in our communities, and give people
+         dignified ways of earning passive income.`
+      }
+    }
+
+
 }
